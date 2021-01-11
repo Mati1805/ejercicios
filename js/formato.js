a53f894981fbd254bfa7ba4ef13bd1d3e10fd5f2
Mati1805 <75944430+Mati1805@users.noreply.github.com>
2021-01-11 20:40:54 -0300

    Add files via upload

---
+++ js/formato.js
@@ -0,0 +1,8 @@
+alert('A continuación deberá ingresar dos números para realizar una suma, una resta, una multiplicación y una división, estas mismas se imprimirán en pantalla')
+var numero1 = parseInt(prompt('Íngrese el primer número: '))
+var numero2 = parseInt(prompt('Íngrese el segundo número mayor que 0 (cero): '))
+var suma = numero1 + numero2
+var resta = numero1 - numero2
+var multiplicacion = numero1 * numero2
+var division = numero1 / numero2
+var peliculas = ([' Capitán América: El soldado del invierno ',' Avengers Endgame ',' Lucifer ',' Yo antes de tí ',' El stand de los besos '].sort());
